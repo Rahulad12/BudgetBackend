bd053a8eba9d6fb58aa1bc4e03e64abfcafe6ecc
Rahul Adhikari <rahul077@academiacollege.edu.np>
2025-04-08 01:12:28 +0545

    Fix: utils

--- utils/updateMonthlyBalance.js
+++ utils/updateMonthlyBalance.js
@@ -1,47 +1,8 @@
 import mongoose from "mongoose";
-import Expenses from "../model/expenses.js";
+import Expenses from "../model/Expenses.js";
 import Income from "../model/Income.js";
 import MonthlyTransaction from "../model/monthyTransaction.js";
 
-// export const updateMonthlyBalance = async (userId, date, transactionId) => {
-//     const objectUserId = new mongoose.Types.ObjectId(userId);
-//     const month = date.getMonth() + 1; // Jan = 0
-//     const year = date.getFullYear();
-
-//     const start = new Date(Date.UTC(year, month - 1, 1, 0, 0, 0, 0));
-//     const end = new Date(Date.UTC(year, month, 0, 23, 59, 59, 999));
-
-//     // Fetch total income
-//     const incomes = await Income.aggregate([
-//         { $match: { userId: objectUserId, date: { $gte: start, $lte: end } } },
-//         { $group: { _id: null, total: { $sum: "$amount" } } }
-//     ]);
-
-//     // Fetch total expense
-//     const expenses = await Expenses.aggregate([
-//         { $match: { userId: objectUserId, date: { $gte: start, $lte: end } } },
-//         { $group: { _id: null, total: { $sum: "$expenseAmount" } } }
-//     ]);
-
-//     const newTransactionId = new mongoose.Types.ObjectId(transactionId);
-//     const incomeTotal = incomes[0]?.total || 0;
-//     const expenseTotal = expenses[0]?.total || 0;
-
-//     const balance = incomeTotal - expenseTotal;
-
-//     // Update transaction record
-//     await MonthlyTransaction.updateMany(
-//         {
-//             userId,
-//             monthYear: `${month.toString().padStart(2, '0')}-${year}`,
-//         },
-//         [
-//             { $set: { monthlyBalance: balance, monthlyIncome: incomeTotal, monthlyExpense: expenseTotal } },
-//             { $push: { transaction: newTransactionId } }
-//         ],
-//         { upsert: true, new: true }
-//     );
-// };
 export const updateMonthlyBalance = async (userId, date) => {
     const objectUserId = new mongoose.Types.ObjectId(userId);
     const month = date.getMonth() + 1; // Jan = 0
